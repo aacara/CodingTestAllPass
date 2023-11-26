a6d405830573c2e266c327d8c49430d047dddf03
aacara <budgielover@naver.com>
2023-11-26 18:40:50 +0900

    week1 2023-11-26 김현지

---
+++ Week1/삼각달팽이/삼각달팽이_김현지.java
@@ -0,0 +1,74 @@
+package Week1.삼각 달팽이;
+
+
+class 삼각달팽이_김현지 {
+    static int[][] stair;
+    static int row;
+    static int col;
+    static int val;
+    static int num;
+    
+    public int[] solution(int n) {
+        num = n;
+        val = 1;
+        
+        makeEmptyStair(n);
+        fillStair(n);
+        
+        int[] answer = {};
+        answer = combineStair(n, answer);
+        return answer;
+    }
+    
+    static void fillStair(int n) {
+        row = -1;
+        col = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 3 == 0) {
+                moveDown();
+            } else if (i % 3 == 1) {
+                moveRight();
+            } else {
+                moveLeftDiagonal();
+            }
+            num--;
+        }
+    }
+    
+    static void moveDown() {
+        for (int i = 0; i < num; i++) {
+            stair[++row][col] = val++;
+        }
+    }
+    static void moveRight() {
+        for (int i = 0; i < num; i++) {
+            stair[row][++col] = val++;
+        }
+    }
+    static void moveLeftDiagonal() {
+        for (int i = 0; i < num; i++) {
+            stair[--row][--col] = val++;
+        }
+    }
+    
+    
+    static void makeEmptyStair(int n) {
+        stair = new int[n][];
+        for (int i = 0; i < n; i++) {
+            stair[i] = new int[i+1];
+        }
+    }
+    
+    static int[] combineStair(int n, int[] answer) {
+        int totalNum = n * (n + 1) / 2;
+        answer = new int[totalNum];
+        
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                answer[idx++] = stair[i][j];
+            }
+        }
+        return answer;
+    }
+}
---
+++ Week1/행렬의곱셈/행렬의곱셈_김현지.java
@@ -0,0 +1,27 @@
+package Week1.행렬의곱셈;
+
+class 행렬의곱셈_김현지 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int x1 = arr1.length;
+        int y1 = arr1[0].length;
+        int x2 = arr2.length;
+        int y2 = arr2[0].length;
+
+        
+        int[][] answer = new int[x1][y2];
+        int element;
+        
+        for (int i = 0 ; i < x1; i++) {
+            for (int k = 0; k < y2; k++) {
+                 element = 0;
+                for (int j = 0; j < y1; j++) {
+                    element += arr1[i][j] * arr2[j][k];
+                    //stem.out.println("i: "+i+ " j: "+ j+ " k: "+ k);
+                }
+                answer[i][k] = element;
+            }
+        }
+        
+        return answer;
+    }
+}
