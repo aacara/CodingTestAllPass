d8651c8b3663a728a292cfde0ba1b6b706c3ad6c
김찬규 <73059667+cgk95@users.noreply.github.com>
2023-12-12 10:01:49 +0000

    Create 문자열다루기기본_김찬규.java

---
+++ Week4/문자열다루기기본/문자열다루기기본_김찬규.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(String s) {
+        if(s.length()!=4 && s.length()!=6) {
+            return false;
+        }
+        for(char c : s.toCharArray()){
+            if(!Character.isDigit(c)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
