a015f59f852c8cf2e7841b1a43b05c17c43c84d4
aacara <budgielover@naver.com>
2024-01-08 11:10:11 +0900

    Week7 2024-01-07 김현지

--- week7/H-Index/H-Index_김현지.java
+++ week7/H-Index/H-Index_김현지.java
@@ -15,8 +15,7 @@ class H-Index_김현지 {
         int citationsLen = citations.length;
         
         for (int i = 0; i < citationsLen; i++) {
-            int h = Math.min(citations[i], citationsLen - i);
-            if (h >= citationsLen - i) {
+            if (citations[i] >= citationsLen - i) {
                 return citationsLen - i;
             }
         }
