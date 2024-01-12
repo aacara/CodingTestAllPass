55fd41bfca419e48ae0cb6ccb61cc005ac1b48d5
김찬규 <73059667+cgk95@users.noreply.github.com>
2024-01-12 06:46:42 +0000

    Update 문자열_내림차순으로_정렬하기_김찬규.java

--- week8/문자열내림차순으로정렬하기/문자열_내림차순으로_정렬하기_김찬규.java
+++ week8/문자열내림차순으로정렬하기/문자열_내림차순으로_정렬하기_김찬규.java
@@ -1,1 +1,7 @@
-ㅁ
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        System.out.println(sb.reverse());
+        return sb.toString();
+    }
+}
