6291c12a7405a047b8002708e18c80381f523da0
aacara <budgielover@naver.com>
2024-01-15 23:09:52 +0900

    Week9 2024-01-15 김현지

---
+++ Week8/가장 큰 수/가장큰수_김현지.java
@@ -0,0 +1,23 @@
+package Week8.가장 큰 수;
+
+import java.util.*;
+
+class Solution {
+    public int[] 가장큰수_김현지(int[] array) {
+        int[] beforeArray  = array.clone();
+        Arrays.sort(array);
+        int arrLen = array.length;
+        int largestNum = array[arrLen - 1];
+        int largestIdx = 0;
+        
+        for (int i = 0; i < arrLen; i++) {
+            if (beforeArray[i] == largestNum) {
+                largestIdx = i;
+                break;
+            }
+        }
+        
+        int[] answer = {largestNum, largestIdx};
+        return answer;
+    }
+}
