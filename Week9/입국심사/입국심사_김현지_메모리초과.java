2befa536a5b2a6e21b030963b11b03276d738649
aacara <budgielover@naver.com>
2024-01-29 18:58:07 +0900

    Week10 2024-01-23 김현지

--- Week9/입국심사/입국심사_김현지_메모리초과.java
+++ Week9/입국심사/입국심사_김현지_메모리초과.java
@@ -1,5 +1,9 @@
 import java.util.*;
 
+// 가능한 모든 경우의 수를 저장
+// 정렬
+// n-1번째 수를 구하면된다고 생각했지만
+// "모든" 경우의 수를 저장해야하기 때문에 메모리 낭비가 심함
 class 입국심사_김현지_메모리초과 {
     private List<Integer> possibleCases = new ArrayList<>();
     
