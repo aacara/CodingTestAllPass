112cef22a39b3920e55207281d957cfae8005b0f
aacara <budgielover@naver.com>
2024-02-07 22:14:28 +0900

    Week12 2024-02-05 김현지

--- Week11/피보나치 수/피보나치_수_김현지.java
+++ Week11/피보나치 수/피보나치_수_김현지.java
@@ -5,9 +5,10 @@ class 피보나치_수_김현지 {
         int [] dp = new int[n+1];
         dp[0] = 0;
         dp[1] = 1;
+        final int MOD = 1_234_567;
         
         for (int i = 2; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
         }
         
         return dp[n];
